c61edb034eaa5fdcd9232e31e8dab9e6886515f6
Drew <drew.michael09@gmail.com>
2023-01-03 19:35:46 -0500

    Completed Section 18 of Vue MasterClass

---
+++ job-search/src/views/JobResultsView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="flex w-full flex-row flex-nowrap">
+    <job-filters-sidebar />
+    <job-listings />
+  </div>
+</template>
+
+<script>
+import JobFiltersSidebar from "@/components/JobResults/JobFilterSidebar/JobFilterSidebar.vue";
+import JobListings from "@/components/JobResults/JobListings.vue";
+
+export default {
+  name: "JobResultsView",
+  components: {
+    JobFiltersSidebar,
+    JobListings,
+  },
+  methods: {
+    goToHome() {
+      this.$router.push({ name: "Home" });
+    },
+  },
+};
+</script>
